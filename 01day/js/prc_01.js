2e7772a785819b502feef8655b17bab78d91efec
JinminYANG <38518818+JinminYANG@users.noreply.github.com>
2020-09-16 23:51:50 +0900

    javascript practice

---
+++ 01day/js/prc_01.js
@@ -0,0 +1,10 @@
+var dog = "가을"; // 일반적인 변수의 선언
+
+// 객체는 많은 이름(neme)과 값(value)으로 구성된 속성(property)의 집합
+var puppy = {name: "가을", age: 3, weight: 700};
+
+document.write("강아지의 이름: " + dog + "<br><br>");
+
+document.write("강아지의 이름: " + puppy.name + "<br>");
+document.write("강아지의 나이: " + puppy.age + "<br>");
+document.write("강아지의 몸무게: " + puppy.weight + "<br>");
